4b45903c52f61003102c45a99a698a7486ea6d65
fector101 <fabianjoseph063@gmail.com>
2025-03-15 12:13:58 +0100

    first interface

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -1,3 +1,19 @@
-import axios from 'axios';
-const url = 'https://jsonplaceholder.typicode.com/todos/1';
-axios.get(url).then(res=>console.log(res.data));
+import axios from "axios";
+
+const url = "https://jsonplaceholder.typicode.com/todos/1";
+
+interface TODO {
+    id: number;
+    title: string;
+    completed: boolean;
+}
+
+axios.get(url).then((res) => {
+    const todo = res.data as TODO;
+    const { id, title, completed } = todo;
+    console.log(`
+        The TODO with ID: ${id}
+        Has a title of: ${title}
+        Is it finished? ${completed}
+        `);
+});
