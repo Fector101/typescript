83ddcc15e5cd66f843744e24de82a495f404020d
fector101 <fabianjoseph063@gmail.com>
2025-03-15 12:59:02 +0100

    vars with types

---
+++ features/annotations/variables.ts
@@ -0,0 +1,10 @@
+let apples: number = 3;
+// apples = '';
+apples = 2;
+let speed: string = 'fast';
+let has_name: boolean = false;
+let nothing_much: null = null;
+let nothing: undefined = undefined;
+
+// built in objects
+let now: Date = new Date()
