e9cf6dc9a24445a463573a7175fd192bcb7f0546
fector101 <fabianjoseph063@gmail.com>
2025-03-16 00:24:15 +0100

    more on functions annonations

--- features/annotations/functions.ts
+++ features/annotations/functions.ts
@@ -1,3 +1,30 @@
 const add = (a: number, b: number): number => {
     return a + b;
 };
+
+function divide(a: number, b: number): number {
+    return a / b;
+}
+
+const logger = (msg: string): void => {
+    console.log(msg);
+};
+
+const forecast = {
+    date: new Date(),
+    weather: 'sunny'
+}
+
+const logWeather = (forecast: { date: Date, weather: string }): void => {
+    console.log(forecast.date);
+    console.log(forecast.weather);
+}
+
+// ES2015
+
+const logWeather1 = ({ date, weather }: { date: Date, weather: string }): void => {
+    console.log(date);
+    console.log(weather);
+}
+
+logWeather1(forecast)
