27592a75c60211ca8fdc690f66b192781d3a5117
fector101 <fabianjoseph063@gmail.com>
2025-03-16 17:47:30 +0100

    working map

--- maps-project/src/index.ts
+++ maps-project/src/index.ts
@@ -1,8 +1,12 @@
 import { Company } from "./Company";
 import { User } from "./User";
 
-const user = new User()
+const user = new User();
 console.log(user);
-const company = new Company()
+const company = new Company();
 console.log(company);
-console.log(google);
+// console.log(google);
+new google.maps.Map(document.getElementById("map"), {
+    zoom: 1,
+    center: { lat: 0, lng: 0 },
+});
