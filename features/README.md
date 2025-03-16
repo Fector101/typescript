74c3f1b2b480a1f554f9b4050655d63e7eadc2be
fector101 <fabianjoseph063@gmail.com>
2025-03-16 02:47:57 +0100

    update README and classes to enhance constructor usage and access modifiers

--- features/README.md
+++ features/README.md
@@ -2,6 +2,28 @@
 
 ## Classes
 
-public - This method can be called any where any time
-private - This method can only be called by other methods in this class
-protected - This method can only be called by other methods in this class or by methods in child class
+public - This attributes\methods can be called any where any time
+private - This attributes\methods can only be called by other methods in this class
+protected - This attributes\methods can only be called by other methods in this class or by methods in child class
+
+## Init Arg
+
+Instead Of This ❌
+
+```ts
+class Vehicle {
+    color: string;
+    constructor(color: string) {
+        this.color = color;
+    }
+}
+```
+
+Do This Instead ✅
+
+```ts
+class Vehicle {
+    constructor(public color: string) { }
+}
+
+```
--- features/classes.ts
+++ features/classes.ts
@@ -1,4 +1,9 @@
 class Vehicle {
+    constructor(
+        public color: string,
+        protected weigh: number,
+        private owner: string
+    ) {}
     drive(): void {
         console.log("driving....");
     }
@@ -6,7 +11,8 @@ class Vehicle {
         // All methods have `public` by default
         console.log("beep beepp");
     }
-    protected showLights(): void { // Child Classes Can Use
+    protected showLights(): void {
+        // Child Classes Can Use
         console.log("Showing Lights...");
     }
     private childCantClassesusetoo(): void {
@@ -14,12 +20,16 @@ class Vehicle {
     }
 }
 
-const vehicle = new Vehicle();
+// const vehicle = new Vehicle();
 // vehicle.drive()
 // vehicle.honk()
 
 // Type of Vehicle
 class Car1 extends Vehicle {
+    constructor(public wheels: number, color: string) {
+        //No public added the `color` parament because don't want to create a new attr `color`, i.e it belongs to `Vehicle`
+        super(color, 333, "Fabian");
+    }
     drive(): void {
         // Override Super method drive
         console.log("vroom");
@@ -34,7 +44,10 @@ class Car1 extends Vehicle {
         // this.childCantClassesusetoo()
     }
 }
-const car_1 = new Car1();
+const car_1 = new Car1(4, "orange");
 car_1.drive();
 car_1.honk();
 car_1.startDoingStuff();
+console.log(car_1.color);
+// console.log(car_1.owner);
+// console.log(car_1.weigh);
