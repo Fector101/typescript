9e91a5466d8f8ae0600ee4d49cb820e8e6de52e2
Fector101 <fabianjoseph063@gmail.com>
2025-07-18 13:10:55 -0700

    Sorter V2 syntax(abstract class)

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,7 +1,9 @@
-import { CharacterCollection } from "./v1/collections/CharactersCollection";
-import { LinkedList } from "./v1/collections/LinkedList";
-import { NumbersCollection } from "./v1/collections/NumbersCollection";
-import { Sorter } from "./v1/Sorter";
+// import { CharacterCollection } from "./v1/collections/CharactersCollection";
+// import { LinkedList } from "./v1/collections/LinkedList";
+// import { NumbersCollection } from "./v1/collections/NumbersCollection";
+// import { Sorter } from "./v1/Sorter";
+
+// V1--------
 
 // const numbersCollection = new NumbersCollection([5, 6, -10, 56, 23, -5, 33])
 // const sorter = new Sorter(numbersCollection)
@@ -13,13 +15,36 @@ import { Sorter } from "./v1/Sorter";
 // sorter1.sort()
 // console.log(characterCollection.data);
 
+// const linkedList = new LinkedList()
+// linkedList.add(550)
+// linkedList.add(-10)
+// linkedList.add(-3)
+// linkedList.add(4)
+// linkedList.add(9)
+
+// const sorter2 = new Sorter(linkedList)
+// sorter2.sort()
+// linkedList.print()
+
+// V2--------
+
+import { CharacterCollection } from "./v2/CharactersCollection";
+import { LinkedList } from "./v2/LinkedList";
+import { NumbersCollection } from "./v2/NumbersCollection";
+const numbersCollection = new NumbersCollection([5, 6, -10, 56, 23, -5, 33])
+numbersCollection.sort()
+console.log(numbersCollection.data)
+
+
 const linkedList = new LinkedList()
 linkedList.add(550)
 linkedList.add(-10)
 linkedList.add(-3)
 linkedList.add(4)
 linkedList.add(9)
-
-const sorter2 = new Sorter(linkedList)
-sorter2.sort()
+linkedList.sort()
 linkedList.print()
+
+const characterCollection = new CharacterCollection('Fabian')
+characterCollection.sort()
+console.log(characterCollection.data);
