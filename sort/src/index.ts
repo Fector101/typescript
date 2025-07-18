f955984918dbab06293d3e21b831553e334e0646
Fector101 <fabianjoseph063@gmail.com>
2025-07-18 11:43:57 -0700

    Sorter V1 syntax(interfaces)

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,7 +1,7 @@
-import { CharacterCollection } from "./collections/CharactersCollection";
-import { LinkedList } from "./collections/LinkedList";
-import { NumbersCollection } from "./collections/NumbersCollection";
-import { Sorter } from "./Sorter";
+import { CharacterCollection } from "./v1/collections/CharactersCollection";
+import { LinkedList } from "./v1/collections/LinkedList";
+import { NumbersCollection } from "./v1/collections/NumbersCollection";
+import { Sorter } from "./v1/Sorter";
 
 // const numbersCollection = new NumbersCollection([5, 6, -10, 56, 23, -5, 33])
 // const sorter = new Sorter(numbersCollection)
